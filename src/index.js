d9d34e338f869fb73aee53025ca51df30dbb981c
Stephan Petzl <stephan.petzl@gmail.com>
2021-03-11 11:16:52 +0100

    Fix tessdata path for electron

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 const path = require('path')
-
-const tessdataPath = path.resolve(__dirname,"..","tessdata")
-process.env.TESSDATA_PREFIX = tessdataPath
-
 let bindings
 
 const isElectron = process.versions.hasOwnProperty('electron')
 
 if(isElectron) {
-  const modulePath = require("electron").remote.app.getAppPath()+'/node_modules/node-native-ocr/build/Release/node-native-ocr'
+
+  const appPath = require("electron").remote.app.getAppPath()
+  const ocrPackagePath = 'node_modules/node-native-ocr'
+  const modulePath = path.resolve(appPath, ocrPackagePath, 'build/Release/node-native-ocr')
   console.log('modulePath', modulePath)
+  const tessdataPath = path.resolve(ocrPackagePath, "tessdata")
+  process.env.TESSDATA_PREFIX = tessdataPath
+  console.log('tessdataPath', tessdataPath)
   bindings = __non_webpack_require__(modulePath)
+  
 } else {
+
+  const tessdataPath = path.resolve(__dirname,"..","tessdata")
+  process.env.TESSDATA_PREFIX = tessdataPath
   bindings = require('../build/Release/node-native-ocr.node')
 }
 
