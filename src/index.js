e5bf39e59336cefb2cad15d334b5cab748cd7898
Stephan Petzl <stephan.petzl@gmail.com>
2026-02-25 18:02:46 +0800

    Migrate to bundled prebuild loading and release workflow

--- src/index.js
+++ src/index.js
@@ -1,39 +1,18 @@
 const path = require('path')
-let bindings
-
-const isElectron = process.versions.hasOwnProperty('electron')
-const ocrPackagePath = 'node_modules/node-native-ocr'
-
-if (isElectron) {
-  const electron = require("electron")
-  const appPath = (electron.app || electron.remote.app).getAppPath()
-  const modulePath = path.resolve(appPath, ocrPackagePath, 'build/Release/node-native-ocr')
-  bindings = __non_webpack_require__(modulePath)
-} else {
-  bindings = require('../build/Release/node-native-ocr.node')
-}
-
+const packageRootPath = path.resolve(__dirname, '..')
+const bindings = require('node-gyp-build')(packageRootPath)
 
 const DEFAULT_LANG = 'eng'
 const LANG_DELIMITER = '+'
 
 const handleOptions = (options = {}) => {
-
-  if(!options.lang){
+  if (!options.lang) {
     options.lang = DEFAULT_LANG
   }
-  if(!options.tessdataPath){
-    if (isElectron) {
-      console.log('Electron mode.')
-      const electron = require("electron")
-      const appPath = (electron.app || electron.remote.app).getAppPath()
-      options.tessdataPath = path.resolve(appPath, ocrPackagePath, 'tessdata')
-    } else{
-      console.log('Node mode.')
-      options.tessdataPath = path.resolve(__dirname, "..", "tessdata")
-    }
+  if (!options.tessdataPath) {
+    options.tessdataPath = path.resolve(packageRootPath, 'tessdata')
   }
-  if(!options.format){
+  if (!options.format) {
     options.format = 'txt'
   }
 
@@ -44,24 +23,23 @@ const handleOptions = (options = {}) => {
   return options
 }
 
-const makePromise = (method) => {
+const makePromise = method => {
+  return (arg, options) =>
+    new Promise((resolve, reject) => {
+      options = handleOptions(options)
 
-  return (arg, options) => new Promise((resolve, reject) => {
-    options = handleOptions(options)
-    
-    bindings[method](arg, options.lang, options.tessdataPath, options.format !== 'txt', (err, text) => {
-      if (err) {
-        console.log('error:', err)
-        const error = new Error(text)
-        error.code = err
-        return reject(error)
-      } else {
-        console.log('success:', text)
-      }
-
-      resolve(text.trim())
+      bindings[method](arg, options.lang, options.tessdataPath, options.format !== 'txt', (err, text) => {
+        if (err) {
+          //console.log('error:', err)
+          const error = new Error(err.message || 'Tesserat error occured.')
+          error.code = err.code
+          reject(error)
+        } else {
+          //console.log('success:', text)
+          resolve(text.trim())
+        }
+      })
     })
-  })
 }
 
 exports.recognize = makePromise('recognize')
