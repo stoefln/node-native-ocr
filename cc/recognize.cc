0c2ddd5a7a5342538b18c3e66d90809a5d2e05ba
Stephan Petzl <stephan.petzl@gmail.com>
2026-03-18 10:07:07 +0800

    Run Windows native OCR on the main thread

--- cc/recognize.cc
+++ cc/recognize.cc
@@ -2,7 +2,9 @@
 #include <uv.h>
 #include <stdint.h>
 #include <cstdio>
+#include <cstring>
 #include <fstream>
+#include <string>
 #include <vector>
 #ifdef _WIN32
 #include <windows.h>
@@ -16,6 +18,75 @@ using Napi::Function;
 using Napi::HandleScope;
 using Napi::String;
 
+Pix *ReadImageFromBuffer(const uint8_t *buffer, size_t length)
+{
+#ifdef _WIN32
+  char tempPath[MAX_PATH];
+  if (GetTempPathA(MAX_PATH, tempPath) > 0)
+  {
+    char tempFile[MAX_PATH];
+    if (GetTempFileNameA(tempPath, "ocr", 0, tempFile) > 0)
+    {
+      std::ofstream tempStream(tempFile, std::ios::binary | std::ios::out | std::ios::trunc);
+      if (tempStream.is_open())
+      {
+        tempStream.write(reinterpret_cast<const char *>(buffer), static_cast<std::streamsize>(length));
+        tempStream.close();
+        Pix *image = pixRead(tempFile);
+        DeleteFileA(tempFile);
+        return image;
+      }
+      DeleteFileA(tempFile);
+    }
+  }
+#endif
+  return pixReadMem(buffer, length);
+}
+
+bool RecognizeBuffer(const uint8_t *buffer,
+                     size_t length,
+                     const std::string &lang,
+                     const std::string &path,
+                     bool tsvOutput,
+                     std::string *result,
+                     std::string *errorCode)
+{
+  Pix *image = ReadImageFromBuffer(buffer, length);
+  if (image == nullptr)
+  {
+    *errorCode = "ERR_READ_IMAGE";
+    return false;
+  }
+
+  char tessErrorCode[50] = {0};
+  char tessErrorMessage[200] = {0};
+  char *outText = nullptr;
+  const int tessFailed = TessRecognizePix(
+      image,
+      lang.c_str(),
+      path.c_str(),
+      tsvOutput,
+      outText,
+      tessErrorCode,
+      tessErrorMessage);
+
+  if (tessFailed != 0)
+  {
+    if (tessErrorMessage[0] != '\0')
+    {
+      fprintf(stderr, "%s", tessErrorMessage);
+      fflush(stderr);
+    }
+    *errorCode = tessErrorCode[0] == '\0' ? "ERR_INIT_TESSER" : tessErrorCode;
+    delete[] outText;
+    return false;
+  }
+
+  result->assign(outText == nullptr ? "" : outText);
+  delete[] outText;
+  return true;
+}
+
 class RecognizeWorker : public Napi::AsyncWorker
 {
 
@@ -24,32 +95,7 @@ private:
   std::string _lang;
   std::string _path;
   bool _tsvOutput;
-  char *_outText = nullptr;
-
-  Pix *ReadImage()
-  {
-#ifdef _WIN32
-    char tempPath[MAX_PATH];
-    if (GetTempPathA(MAX_PATH, tempPath) > 0)
-    {
-      char tempFile[MAX_PATH];
-      if (GetTempFileNameA(tempPath, "ocr", 0, tempFile) > 0)
-      {
-        std::ofstream tempStream(tempFile, std::ios::binary | std::ios::out | std::ios::trunc);
-        if (tempStream.is_open())
-        {
-          tempStream.write(reinterpret_cast<const char *>(_buffer.data()), static_cast<std::streamsize>(_buffer.size()));
-          tempStream.close();
-          Pix *image = pixRead(tempFile);
-          DeleteFileA(tempFile);
-          return image;
-        }
-        DeleteFileA(tempFile);
-      }
-    }
-#endif
-    return pixReadMem(_buffer.data(), _buffer.size());
-  }
+  std::string _result;
 
 public:
   RecognizeWorker(const uint8_t *buffer,
@@ -67,29 +113,15 @@ public:
   void OnOK() override
   {
     HandleScope scope(Env());
-    Callback().Call({Env().Null(), String::New(Env(), _outText == nullptr ? "" : _outText)});
-    delete[] _outText;
-    _outText = nullptr;
+    Callback().Call({Env().Null(), String::New(Env(), _result)});
   }
 
   void Execute() override
   {
-    Pix *image = ReadImage();
-
-    if (image == nullptr)
+    std::string errorCode;
+    if (!RecognizeBuffer(_buffer.data(), _buffer.size(), _lang, _path, _tsvOutput, &_result, &errorCode))
     {
-      SetError("ERR_READ_IMAGE");
-      return;
-    }
-    char error_code[50];
-    char error_message[200];
-
-    int tess_failed = TessRecognizePix(image, _lang.c_str(), _path.c_str(), _tsvOutput, _outText, error_code, error_message);
-
-    if (tess_failed)
-    {
-      fprintf(stderr, "%s", error_message);
-      SetError(error_code);
+      SetError(errorCode);
       return;
     }
   }
@@ -154,6 +186,20 @@ void Recognize(const Napi::CallbackInfo &info)
   // printf("\n\nlang: %s\n", lang.c_str());
 
   _getBufferInfo(info[0].ToObject(), (void **)(&bufferData), &bufferLength);
+
+#ifdef _WIN32
+  std::string result;
+  std::string errorCode;
+  if (!RecognizeBuffer(bufferData, bufferLength, lang, tessDataPath, tsvOutput, &result, &errorCode))
+  {
+    callback.Call({Napi::Error::New(env, errorCode).Value()});
+    return;
+  }
+
+  callback.Call({env.Null(), String::New(env, result)});
+  return;
+#endif
+
   RecognizeWorker *asyncWorker = new RecognizeWorker(
       bufferData,
       bufferLength,
